ff4c4c1e04706b5829081fddc5b6a65398f76d14
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-17 21:41:44 -0500

    feat: menu option for bc01

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/turret-io/go-menu/menu"
 
+	"github.com/AlexFBP/backphish/bc01"
 	"github.com/AlexFBP/backphish/dp01"
 )
 
 func main() {
 	commandOptions := []menu.CommandOption{
 		menu.CommandOption{"dp1", "attack fake daviplata 1", dp01.Cmd1},
+		menu.CommandOption{"bc1", "attack fake bancolombia 1", bc01.Cmd},
 	}
 	menuOptions := menu.NewMenuOptions("'menu' for help > ", 0)
 	menu := menu.NewMenu(commandOptions, menuOptions)
