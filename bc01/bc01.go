5f4bffbb3fdc55b4fc3ee6e4ce6a3fea6c158028
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-17 21:48:01 -0500

    feat-bc01: part 1/3

--- bc01/bc01.go
+++ bc01/bc01.go
@@ -1,20 +1,60 @@
 package bc01
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
 	"github.com/AlexFBP/backphish/common"
 )
 
 func Cmd(args ...string) error {
 	return common.AttackRunner(attempt)
+	// For single attack, comment line above and uncomment this:
+	// attempt()
+	// return nil
 }
 
 func attempt() {
-	// POST https://desbloqueo--sucursalvirtua2.repl.co/finish9.php
-	// cedula=224224242424
+	sendReq(
+		// "http://localhost:1080",
+		"https://desbloqueo--sucursalvirtua2.repl.co/finish9.php",
+		map[string]string{"cedula": "224224242424"})
 
 	// POST https://activacion--vitualclave.repl.co/finish9.php
 	// clave=6248
 
 	// POST https://dinamica.vitualclave.repl.co/finish9.php
 	// clave=245871
+}
+
+func sendReq(postUrl string, params map[string]string) {
+	client := &http.Client{}
+	data := url.Values{}
+	for k, v := range params {
+		data.Add(k, v)
+	}
+	req, err := http.NewRequest("POST", postUrl, strings.NewReader(data.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	reqHeaders := map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+	for k, v := range reqHeaders {
+		req.Header.Add(k, v)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	flat, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", flat)
 }
