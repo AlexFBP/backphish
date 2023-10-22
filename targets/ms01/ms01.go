c50717660fbc88d48cee0389ed70758bbe9cc3de
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-22 11:20:03 -0500

    feat-ms01: random values for form

--- targets/ms01/ms01.go
+++ targets/ms01/ms01.go
@@ -1,20 +1,40 @@
 package ms01
 
-import "github.com/AlexFBP/backphish/common"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+
+	"github.com/brianvoe/gofakeit"
+
+	"github.com/AlexFBP/backphish/common"
+)
 
 func Cmd() {
 	attempt()
 }
 
 func attempt() {
+	domains := []string{
+		"hotmail.com",
+		"live.com",
+		"outlook.com",
+	}
+	em := gofakeit.Email()
+	em += fmt.Sprintf("%s@%s", strings.Split(em, "@")[0], domains[rand.Intn(len(domains))])
+	form := map[string]string{
+		"argaml": em,
+		"argapw": gofakeit.Password(gofakeit.Bool(), gofakeit.Bool(), gofakeit.Bool(), gofakeit.Bool(), gofakeit.Bool(), gofakeit.Number(5, 20)),
+	}
+	pin := fmt.Sprintf("%04d", common.GeneraPin(4))
+	form["pn1"] = pin
+	form["pn2"] = pin
+	if gofakeit.Bool() {
+		form["KMSI"] = "on"
+	}
+
 	common.SendPostEncoded("http://recuperacion004.0hi.me/next.php",
-		map[string]string{
-			"argaml": "metanseundedo@inutiles.com",
-			"argapw": "carevergas",
-			"pn1":    "3698",
-			"pn2":    "3698",
-			"KMSI":   "on",
-		},
+		form,
 		map[string]string{
 			"Host":    "recuperacion004.0hi.me",
 			"Origin":  "http://recuperacion004.0hi.me",
