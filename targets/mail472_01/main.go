c3040772a545e3e2e4efb1464385d9e5c342c39a
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-21 22:48:36 -0500

    feat-472-1: basic requests

--- targets/mail472_01/main.go
+++ targets/mail472_01/main.go
@@ -1,5 +1,7 @@
 package mail47201
 
+import "github.com/AlexFBP/backphish/common"
+
 func Cmd(args ...string) error {
 	attempt()
 	return nil
@@ -12,9 +14,37 @@ func attempt() {
 
 	// POST https://guianacional4-72.com/actualizar_datos.php
 
-	// POST https://guianacional4-72.com/informacion_pago.php
-	// nombre=Juan&apellido=Perez&cedula=1000101010&telefono=3131313131&email=a%40t.com&ciudad=Cali&direccion=Cali&actualizar_datos=Continuar
+	common.SendPostEncoded(
+		// "http://localhost:1080",
+		"https://guianacional4-72.com/informacion_pago.php",
+		map[string]string{
+			"nombre":           "Juan",
+			"apellido":         "Perez",
+			"cedula":           "1000101010",
+			"telefono":         "3131313131",
+			"email":            "a@t.com",
+			"ciudad":           "Cali",
+			"direccion":        "Cali",
+			"actualizar_datos": "Continuar",
+		},
+		map[string]string{
+			"Origin":  "https://guianacional4-72.com",
+			"Referer": "https://guianacional4-72.com/actualizar_datos.php",
+		},
+	)
 
-	// POST https://guianacional4-72.com/comprobando.php
-	// codigo=4573207634320360&fecha=07%2F25&cvv=509&enviar=Pagar+Servicio
+	common.SendPostEncoded(
+		// "http://localhost:1080",
+		"https://guianacional4-72.com/comprobando.php",
+		map[string]string{
+			"codigo": "4573207634320360",
+			"fecha":  "07/25",
+			"cvv":    "509",
+			"enviar": "Pagar Servicio",
+		},
+		map[string]string{
+			"Origin":  "https://guianacional4-72.com",
+			"Referer": "https://guianacional4-72.com/informacion_pago.php",
+		},
+	)
 }
