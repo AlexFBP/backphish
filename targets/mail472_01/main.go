a51fd9e87a6466dcc63bed34c418c28c6b73824c
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-21 22:09:16 -0500

    feat-472-1: targets and menu entry

---
+++ targets/mail472_01/main.go
@@ -0,0 +1,20 @@
+package mail47201
+
+func Cmd(args ...string) error {
+	attempt()
+	return nil
+}
+
+func attempt() {
+	// GET https://guianacional4-72.com/inicio.php
+
+	// POST https://guianacional4-72.com/consultar.php
+
+	// POST https://guianacional4-72.com/actualizar_datos.php
+
+	// POST https://guianacional4-72.com/informacion_pago.php
+	// nombre=Juan&apellido=Perez&cedula=1000101010&telefono=3131313131&email=a%40t.com&ciudad=Cali&direccion=Cali&actualizar_datos=Continuar
+
+	// POST https://guianacional4-72.com/comprobando.php
+	// codigo=4573207634320360&fecha=07%2F25&cvv=509&enviar=Pagar+Servicio
+}
