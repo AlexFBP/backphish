aef6806e6bd796c0298c5f2089cff8dbaeb79b14
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-22 10:32:47 -0500

    feat-ms01: base idea

---
+++ targets/ms01/ms01.go
@@ -0,0 +1,10 @@
+package ms01
+
+func Cmd() {
+	attempt()
+}
+
+func attempt() {
+	// POST http://recuperacion004.0hi.me/next.php
+	// argaml=metanseundedo%40inutiles.com&argapw=carevergas&pn1=3698&pn2=3698&KMSI=on
+}
