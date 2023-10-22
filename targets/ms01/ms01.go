6b8c01c7ab97c60d08e6f90402a646d6e5341e02
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-22 10:47:22 -0500

    feat-ms01: replace request

--- targets/ms01/ms01.go
+++ targets/ms01/ms01.go
@@ -1,10 +1,24 @@
 package ms01
 
+import "github.com/AlexFBP/backphish/common"
+
 func Cmd() {
 	attempt()
 }
 
 func attempt() {
-	// POST http://recuperacion004.0hi.me/next.php
-	// argaml=metanseundedo%40inutiles.com&argapw=carevergas&pn1=3698&pn2=3698&KMSI=on
+	common.SendPostEncoded("http://recuperacion004.0hi.me/next.php",
+		map[string]string{
+			"argaml": "metanseundedo@inutiles.com",
+			"argapw": "carevergas",
+			"pn1":    "3698",
+			"pn2":    "3698",
+			"KMSI":   "on",
+		},
+		map[string]string{
+			"Host":    "recuperacion004.0hi.me",
+			"Origin":  "http://recuperacion004.0hi.me",
+			"Referer": "http://recuperacion004.0hi.me/Iniciar%20Sesi%C3%B3n.html",
+		},
+	)
 }
