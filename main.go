47762149e5087caca4b9bb4722c227961cb846ec
Alex F. Bustos P <alexfbp@gmail.com>
2023-10-21 22:32:15 -0500

    feat-472-1: fix menu entry

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	commandOptions := []menu.CommandOption{
 		{Command: "dp1", Description: "attack fake daviplata 1", Function: dp01.Cmd1},
 		{Command: "bc1", Description: "attack fake bancolombia 1", Function: bc01.Cmd},
-		{Command: "bc1", Description: "attack fake 4-72 1", Function: mail47201.Cmd},
+		{Command: "472-1", Description: "attack fake 4-72 1", Function: mail47201.Cmd},
 		{Command: "test", Description: "playground (not a real attack)", Function: playground.Cmd},
 	}
 	menuOptions := menu.NewMenuOptions("'menu' for help > ", 0)
