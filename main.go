fde1ffdaabcb57e6dc6323a61a140aeb7564631f
Alex F. Bustos P <alexfbp@gmail.com>
2024-07-07 12:33:03 -0500

    feat: skeleton of bc02 added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	// "github.com/AlexFBP/backphish/common"
 	"github.com/AlexFBP/backphish/playground"
 	"github.com/AlexFBP/backphish/targets/bc01"
+	"github.com/AlexFBP/backphish/targets/bc02"
 	"github.com/AlexFBP/backphish/targets/dp01"
 	mail47201 "github.com/AlexFBP/backphish/targets/mail472_01"
 	"github.com/AlexFBP/backphish/targets/ms01"
@@ -17,6 +18,7 @@ func main() {
 		// Attacks - Please sort alphabetically by key
 		{Command: "472-1", Description: "attack fake 4-72 1", Function: mail47201.Cmd},
 		{Command: "bc1", Description: "attack fake bancolombia 1", Function: bc01.Cmd},
+		{Command: "bc2", Description: "attack fake bancolombia 2", Function: bc02.Cmd},
 		{Command: "dp1", Description: "attack fake daviplata 1", Function: dp01.Cmd1},
 		{Command: "ms1", Description: "attack fake MS login 1", Function: ms01.Cmd},
 		// Playground - Please let this one at the end
