6b3e50fcbefce37ba9b7895a45f08c96a4ac11e0
Alex F. Bustos P <alexfbp@gmail.com>
2024-09-03 12:30:49 -0500

    nf1-feat: requests implemented

--- targets/netflix1/main.go
+++ targets/netflix1/main.go
@@ -1,43 +1,71 @@
 package netflix1
 
+import (
+	"github.com/AlexFBP/backphish/common"
+)
+
 func Cmd(args ...string) error {
-	// return common.AttackRunner(attempt, common.ArgsHaveTimes(args...))
-	// For single attack, comment line above and uncomment this:
-	attempt()
-	return nil
+	return common.AttackRunner(attempt, common.ArgsHaveTimes(args...))
 }
 
 func attempt() {
-	// GET https://mi-cuentasuscripcionflix.com/
-	// to catch in response headers:
-	// set-cookie - PHPSESSID=...; path=/
-	// ----> To be used in subsequent requests in Cookie header
+	h := common.ReqHandler{}
+	h.UseJar(true)
 
-	// POST https://mi-cuentasuscripcionflix.com/captcha/set_captcha_session.php
-	// (origin: https://mi-cuentasuscripcionflix.com)
-	// (referer: https://mi-cuentasuscripcionflix.com/captcha/captcha.php)
-	// form (content-type: application/x-www-form-urlencoded; charset=UTF-8) with values:
-	// captcha: true
+	h.SendGet(
+		"https://mi-cuentasuscripcionflix.com/",
+		map[string]string{}, map[string]string{}, nil,
+	)
 
-	// POST https://mi-cuentasuscripcionflix.com/send.php
-	// (origin: https://mi-cuentasuscripcionflix.com)
-	// (referer: https://mi-cuentasuscripcionflix.com/index.php)
-	// form with FAKE values:
-	// username: care@hotmail.com
-	// password: datsacoolpassword
+	// Maybe all the following is not needed
+	// u, _ := url.Parse("https://mi-cuentasuscripcionflix.com")
+	// const COOKIE_NAME = "PHPSESSID"
+	// sess := ""
+	// for _, cookie := range h.Jar.Cookies(u) {
+	// 	fmt.Printf("%s\t%s\n", cookie.Name, cookie.Value)
+	// 	if cookie.Name == COOKIE_NAME {
+	// 		sess = cookie.Value
+	// 	}
+	// }
 
-	// POST https://mi-cuentasuscripcionflix.com/send.php
-	// (origin: https://mi-cuentasuscripcionflix.com)
-	// (referer: https://mi-cuentasuscripcionflix.com/billing.php)
-	// form with FAKE values:
-	// name: Kaarlo Negassi
-	// adresse: 800 Howard Baker Jr. Ave
-	// ville: Knoxville
-	// zip: 37915
-	// tel: 8652157450
-	// dob: 08/02/1989
-	// ccc: 4342 4437 0984 1952
-	// exp: 10/29
-	// cvc: 671
-	// titulaire: Emin Devari
+	h.SendPostEncoded(
+		"https://mi-cuentasuscripcionflix.com/captcha/set_captcha_session.php",
+		map[string]string{"captcha": "true"},
+		map[string]string{
+			"Origin":  "https://mi-cuentasuscripcionflix.com",
+			"Referer": "https://mi-cuentasuscripcionflix.com/captcha/captcha.php",
+		}, nil,
+	)
+
+	h.SendPostEncoded(
+		"https://mi-cuentasuscripcionflix.com/send.php",
+		map[string]string{
+			"username": "care@hotmail.com",
+			"password": "datsacoolpassword",
+		},
+		map[string]string{
+			"Origin":  "https://mi-cuentasuscripcionflix.com",
+			"Referer": "https://mi-cuentasuscripcionflix.com/index.php",
+		}, nil,
+	)
+
+	h.SendPostEncoded(
+		"https://mi-cuentasuscripcionflix.com/send.php",
+		map[string]string{
+			"name":      "Kaarlo Negassi",
+			"adresse":   "800 Howard Baker Jr. Ave",
+			"ville":     "Knoxville",
+			"zip":       "37915",
+			"tel":       "8652157450",
+			"dob":       "08/02/1989",
+			"ccc":       "4342 4437 0984 1952",
+			"exp":       "10/29",
+			"cvc":       "671",
+			"titulaire": "Emin Devari",
+		},
+		map[string]string{
+			"Origin":  "https://mi-cuentasuscripcionflix.com",
+			"Referer": "https://mi-cuentasuscripcionflix.com/billing.php",
+		}, nil,
+	)
 }
