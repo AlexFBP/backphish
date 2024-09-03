00156d07798ff85b6025bcfe3ee7ef2784a27c2b
Alex F. Bustos P <alexfbp@gmail.com>
2024-09-03 15:59:07 -0500

    nf1-feat: random values for requests

--- targets/netflix1/main.go
+++ targets/netflix1/main.go
@@ -1,6 +1,14 @@
 package netflix1
 
 import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/brianvoe/gofakeit"
+	// fd "github.com/udistrital/utils_oas/formatdata"
+
 	"github.com/AlexFBP/backphish/common"
 )
 
@@ -37,11 +45,19 @@ func attempt() {
 		}, nil,
 	)
 
+	p := gofakeit.Person()
+	l_min := 8
+	l_max := 20
+	// TODO: Choose randomly either a "strong" or a "dictionary" password
+	pass := gofakeit.Password(true, true, true, true, false, rand.Intn(l_max-l_min+1)+l_min)
+	// fd.JsonPrint(p)
+	// fmt.Println(p)
+
 	h.SendPostEncoded(
 		"https://mi-cuentasuscripcionflix.com/send.php",
 		map[string]string{
-			"username": "care@hotmail.com",
-			"password": "datsacoolpassword",
+			"username": p.Contact.Email,
+			"password": pass,
 		},
 		map[string]string{
 			"Origin":  "https://mi-cuentasuscripcionflix.com",
@@ -55,19 +71,24 @@ func attempt() {
 		map[string]string{"Referer": "https://mi-cuentasuscripcionflix.com/billing.php"}, nil,
 	)
 
+	n := time.Now()
+	bd := gofakeit.DateRange(n.AddDate(-70, 0, 0), n.AddDate(-18, 0, 5))
+	cc := strconv.Itoa(p.CreditCard.Number)
+	cc = strings.Join([]string{cc[:4], cc[4:8], cc[8:12], cc[12:]}, " ")
+	// fmt.Println("cardNum:", cc)
 	h.SendPostEncoded(
 		"https://mi-cuentasuscripcionflix.com/send.php",
 		map[string]string{
-			"name":      "Kaarlo Negassi",
-			"adresse":   "800 Howard Baker Jr. Ave",
-			"ville":     "Knoxville",
-			"zip":       "37915",
-			"tel":       "8652157450",
-			"dob":       "08/02/1989",
-			"ccc":       "4342 4437 0984 1952",
-			"exp":       "10/29",
-			"cvc":       "671",
-			"titulaire": "Emin Devari",
+			"name":      p.FirstName + " " + p.LastName,
+			"adresse":   p.Address.Street,
+			"ville":     p.Address.City,
+			"zip":       p.Address.Zip,
+			"tel":       p.Contact.Phone,
+			"dob":       bd.Format("01/02/2006"), // Refer https://pkg.go.dev/time#pkg-constants
+			"ccc":       cc,
+			"exp":       p.CreditCard.Exp,
+			"cvc":       p.CreditCard.Cvv,
+			"titulaire": p.FirstName + " " + p.LastName,
 		},
 		map[string]string{
 			"Origin":  "https://mi-cuentasuscripcionflix.com",
