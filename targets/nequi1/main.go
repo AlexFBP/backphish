e3a098babc4cef232db6011138161c20783ab602
Alex F. Bustos P <alexfbp@gmail.com>
2024-09-09 01:45:57 -0500

    feat-nq1: base queries + final query

--- targets/nequi1/main.go
+++ targets/nequi1/main.go
@@ -9,39 +9,61 @@ func Cmd(args ...string) error {
 }
 
 func attempt() {
-	// POST (+ Preflight!?) https://yousitesureonlineverification.com/recursos/namekusei.php
-	// content-type: application/json
-	// origin: https://finanzasaturitmo.com
-	// referer: https://finanzasaturitmo.com/
-	// {"cedula":"23234654"}
+	h := common.ReqHandler{}
+	h.UseJar(true)
 
-	// POST https://finanzasaturitmo.com/NEQUI/3d/process2/pasousuario.php
-	//     HEADERS:
-	// content-type: application/x-www-form-urlencoded; charset=UTF-8
-	// origin: https://finanzasaturitmo.com
-	// referer: https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/neq.php
-	//     VALUES:
-	// pass: 3075
-	// user: 310 350 5050
-	// dis: PC
-	// banco: NEQUI
+	// POST (+ Preflight!?) https://yousitesureonlineverification.com/recursos/namekusei.php
+	h.SendJSON("https://yousitesureonlineverification.com/recursos/namekusei.php",
+		map[string]string{"cedula": "23234654"},
+		map[string]string{
+			"Origin":  "https://finanzasaturitmo.com",
+			"Referer": "https://finanzasaturitmo.com/",
+		}, nil)
+
+	h.SendPostEncoded(
+		"https://finanzasaturitmo.com/NEQUI/3d/process2/pasousuario.php",
+		map[string]string{
+			"pass":  "3075",
+			"user":  "310 350 5050",
+			"dis":   "PC",
+			"banco": "NEQUI",
+		}, map[string]string{
+			"Origin":  "https://finanzasaturitmo.com",
+			"Referer": "https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/neq.php",
+		}, nil)
 	//     SET-COOKIE:
 	// usuario, contrasena, registro, estado
 
+	status := ""
 	// POST https://finanzasaturitmo.com/NEQUI/3d/process2/estado.php
-	//     HEADERS:
-	// Cookie: usuario=310%20350%205050; contrasena=3075; registro=516; estado=1
-	// content-type: application/json
-	// origin: https://finanzasaturitmo.com
-	// referer: https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/neq.php
 	//     NOTE: This request returns a "status" number as body, related to consultar_estado()
 	// of https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/js/functions2.js
 	// (NO PAYLOAD! - MUST BE REPEATED WHILE REPLY BODY = "1" - should break with 2)
+	h.SendPostEncoded(
+		"https://finanzasaturitmo.com/NEQUI/3d/process2/estado.php", nil,
+		map[string]string{
+			"Origin":  "https://finanzasaturitmo.com",
+			"Referer": "https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/cargando.php",
+		}, &status)
 
-	// POST https://finanzasaturitmo.com/NEQUI/3d/process2/pasoOTP.php
-	//     HEADERS:
-	// content-type: application/x-www-form-urlencoded; charset=UTF-8
-	// Referer:	https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/otp.php
-	// 			VALUES:
-	// otp=726548
+	h.SendPostEncoded(
+		"https://finanzasaturitmo.com/NEQUI/3d/process2/pasoOTP.php",
+		map[string]string{"otp": "726548"},
+		map[string]string{
+			"Origin":  "https://finanzasaturitmo.com",
+			"Referer": "https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/otp.php",
+		}, nil)
+	//     SET-COOKIE:
+	// cdinamica
+
+	// POST https://finanzasaturitmo.com/NEQUI/3d/process2/estado.php
+	//     NOTE: This request returns a "status" number as body, related to consultar_estado()
+	// of https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/js/functions2.js
+	// (NO PAYLOAD! - MUST BE REPEATED WHILE REPLY BODY = "3" - should break with 12)
+	h.SendPostEncoded(
+		"https://finanzasaturitmo.com/NEQUI/3d/process2/estado.php", nil,
+		map[string]string{
+			"Origin":  "https://finanzasaturitmo.com",
+			"Referer": "https://finanzasaturitmo.com/NEQUI/3d/propulsor/nequi/cargando.php",
+		}, &status)
 }
