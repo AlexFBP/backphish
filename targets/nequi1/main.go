5b3845e05a79d392449db3f1004822273aed8ac9
Alex F. Bustos P <alexfbp@gmail.com>
2024-09-11 03:27:03 -0500

    feat-nq1: randomize queries

--- targets/nequi1/main.go
+++ targets/nequi1/main.go
@@ -3,7 +3,10 @@ package nequi1
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/turret-io/go-menu/menu"
 
@@ -61,20 +64,23 @@ func attempt(mirrorPath string) {
 	h := common.ReqHandler{}
 	h.UseJar(true)
 
+	nip := common.GeneraNIPcolombia()
 	// POST (+ Preflight!?) https://yousitesureonlineverification.com/recursos/namekusei.php
 	h.SendJSON("https://yousitesureonlineverification.com/recursos/namekusei.php",
-		map[string]string{"cedula": "23234654"},
+		map[string]string{"cedula": strconv.Itoa(nip)},
 		map[string]string{
 			"Origin":  "https://" + mirrorPath + "",
 			"Referer": "https://" + mirrorPath + "/",
 		}, nil)
 
+	cel := strconv.Itoa(common.GeneraCelColombia())
+	opts := []string{"iPhone", "iPod", "PC"}
 	h.SendPostEncoded(
 		"https://"+mirrorPath+"/NEQUI/3d/process2/pasousuario.php",
 		map[string]string{
-			"pass":  "3075",
-			"user":  "310 350 5050",
-			"dis":   "PC",
+			"pass":  strconv.Itoa(common.GeneraPin(4)),
+			"user":  strings.Join([]string{cel[:3], cel[3:6], cel[6:]}, " "),
+			"dis":   opts[rand.Intn(len(opts))],
 			"banco": "NEQUI",
 		}, map[string]string{
 			"Origin":  "https://" + mirrorPath + "",
@@ -99,7 +105,7 @@ func attempt(mirrorPath string) {
 
 	h.SendPostEncoded(
 		"https://"+mirrorPath+"/NEQUI/3d/process2/pasoOTP.php",
-		map[string]string{"otp": "726548"},
+		map[string]string{"otp": strconv.Itoa(common.GeneraPin(6))},
 		map[string]string{
 			"Origin":  "https://" + mirrorPath + "",
 			"Referer": "https://" + mirrorPath + "/NEQUI/3d/propulsor/nequi/otp.php",
