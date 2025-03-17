f746c1db5c76f70dcbe274ae6c0f0ce518e47e76
Alex F. Bustos P <alexfbp@gmail.com>
2025-03-17 01:13:07 -0500

    feat-nq9: split mirror data and logic, p2/2

--- targets/nequi9/mirror-data.go
+++ targets/nequi9/mirror-data.go
@@ -1,33 +1,5 @@
 package nequi9
 
-import (
-	"github.com/AlexFBP/backphish/common"
-)
-
-type mirrorData struct {
-	common.Discord
-	common.Telegram
-	common.MirrorOptions
-}
-
-func mirrData(name string) (d mirrorData) {
-	for _, v := range mirrors {
-		if name == v[0] {
-			d.Webhook, d.Token, d.Chat = v[1], v[2], v[3]
-			d.ParseOptions(v[4])
-			break
-		}
-	}
-	return
-}
-
-func getMirrorNames() (names []string) {
-	for _, v := range mirrors {
-		names = append(names, v[0])
-	}
-	return
-}
-
 // Mirrors. The comment depending on last checked state:
 //   - (*1): Apparently Down by host provider
 var mirrors = [][]string{
